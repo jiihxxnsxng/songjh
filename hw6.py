9a0cf1006af0de2dfb4840a84b1398f3a8b970c7
jiihxxnsxng <songjh020212@naver.com>
2024-04-19 16:20:48 +0900

    Add Text File[hw6.py]

---
+++ hw6.py
@@ -0,0 +1,16 @@
+for i in range(1, 10):
+    for j in range(2, 6):  
+        re = i * j
+        print(j, 'x', i, '=', re, end='\t')
+        if j == 5:  
+            print()
+
+
+print()  
+
+for i in range(1, 10):
+    for j in range(6, 10):  
+        re = i * j
+        print(j, 'x', i, '=', re, end='\t')
+        if j == 9:  
+            print()
